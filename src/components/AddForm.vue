e807eea79d17085c164cab192be2c8189f4e9e8a
Preina Mariya <preinamaria@gmail.com>
2021-10-27 22:02:18 +0300

    fix form validation

--- src/components/AddForm.vue
+++ src/components/AddForm.vue
@@ -9,7 +9,7 @@
         :class="{'form__input--error' : errors.name}"
         placeholder="Введите наименование товара"
         v-model.trim="requiredFields.name"
-        @blur="checkName"
+        @input="checkName"
       >
       <span class="form__error" v-if="errors.name">{{ errors.name }}</span>
     </div>
@@ -32,7 +32,7 @@
         :class="{'form__input--error' : errors.img}"
         placeholder="Введите ссылку"
         v-model.trim="requiredFields.img"
-        @blur="checkImg"
+        @input="checkImg"
       >
       <span class="form__error" v-if="errors.img">{{ errors.img }}</span>
     </div>
@@ -45,11 +45,11 @@
         :class="{'form__input--error' : errors.price}"
         placeholder="Введите цену"
         v-model.trim="requiredFields.price"
-        @blur="checkPrice"
+        @input="checkPrice"
       >
       <span class="form__error" v-if="errors.price">{{ errors.price }}</span>
     </div>
-    <button type="submit" class="form__btn" :disabled="!isFormEnable">Добавить товар</button>
+    <button type="submit" class="form__btn" :class="{ 'disabled' : !isFormEnable, 'enabled' : isFormEnable }">Добавить товар</button>
   </form>
 </template>
 
@@ -92,15 +92,20 @@ export default {
       this.checkRequiredField(this.requiredFields.price, 'price')
     },
     onSubmit () {
-      const values = this.requiredFields
-      this.$store.commit('addProduct', {
-        name: values.name,
-        description: this.description,
-        img: values.img,
-        price: Number(values.price)
-      })
-      Object.keys(values).forEach(key => { values[key] = '' })
-      this.description = ''
+      this.checkName()
+      this.checkImg()
+      this.checkPrice()
+      if (this.isFormEnable) {
+        const values = this.requiredFields
+        this.$store.commit('addProduct', {
+          name: values.name,
+          description: this.description,
+          img: values.img,
+          price: Number(values.price)
+        })
+        Object.keys(values).forEach(key => { values[key] = '' })
+        this.description = ''
+      }
     }
   }
 }
@@ -114,6 +119,7 @@ export default {
   box-shadow: $shadow-main;
   border-radius: 4px;
   padding: 24px;
+  position: relative;
   @media only screen and (max-width: $bp-medium) {
     padding: 10px;
   }
@@ -193,16 +199,15 @@ export default {
     background-color: $color-btn;
     cursor: pointer;
     transition: all linear .2s;
-    &:enabled {
+    &.enabled {
       -webkit-box-shadow: $shadow-input;
       box-shadow: $shadow-input;
     }
-    &:disabled {
+    &.disabled {
       color: $color-text-placeholder;
       background-color: $color-btn-disable;
-      cursor: not-allowed;
     }
-    &:hover:enabled {
+    &.enabled:hover {
       background-color: transparent;
       color: $color-btn;
       border: 2px solid $color-btn;
